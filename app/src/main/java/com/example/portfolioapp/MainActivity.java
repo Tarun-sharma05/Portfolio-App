a8feee92908bcc141bbf1c529ae65c3557c2743b
Tarun Sharma <tarundadhich05@gmail.com>
2024-03-06 19:15:53 +0530

    First commit

---
+++ app/src/main/java/com/example/portfolioapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.portfolioapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+    Button skills_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
